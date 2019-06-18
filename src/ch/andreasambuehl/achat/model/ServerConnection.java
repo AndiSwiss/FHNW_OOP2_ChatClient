bc1aaae1610c3aa0d0e5c3e8eee2362086eacb32
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-06-18 18:04:43 +0200

    v0.3a rudimentary server connection implemented
    For testing, I just copied some code from the simple chat client
    provided by Mr. Richards.
    
    The communication with the Server seems to work (via the terminal), but
    the GUI get's blocked
    -> so I most likely need to move the connection to a separate thread...

--- src/ch/andreasambuehl/achat/model/ServerConnection.java
+++ src/ch/andreasambuehl/achat/model/ServerConnection.java
@@ -2,8 +2,12 @@ package ch.andreasambuehl.achat.model;
 
 import ch.andreasambuehl.achat.common.ServiceLocator;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerConnection extends Thread {
 
@@ -24,6 +28,47 @@ public class ServerConnection extends Thread {
             socket = new Socket(serverIpAddress, serverPort);
             ServiceLocator.getServiceLocator().getLogger().info("Connected with server " + serverIpAddress
                     + ":" + serverPort);
+
+
+            try (BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 OutputStreamWriter socketOut = new OutputStreamWriter(socket.getOutputStream())) {
+                // Create thread to read incoming messages
+
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            String msg;
+                            try {
+                                msg = socketIn.readLine();
+                                System.out.println("Received: " + msg);
+                            } catch (IOException e) {
+                                break;
+                            }
+                            if (msg == null) break; // In case the server closes the socket
+                        }
+                    }
+                };
+                Thread t = new Thread(r);
+                t.start();
+
+                // Loop, allowing the user to send messages to the server
+                // Note: We still have our scanner
+                System.out.println("Enter commands or enter 'quit'");
+                try (Scanner in = new Scanner(System.in)) {
+                    while (in.hasNext()) {
+                        String line = in.nextLine();
+                        socketOut.write(line + "\n");
+                        socketOut.flush();
+                        System.out.println("Sent: " + line);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
 
         } catch (IOException e) {
             ServiceLocator.getServiceLocator().getLogger().info("Connection with server failed: " + serverIpAddress
